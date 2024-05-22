2026eb055ba88a03703b64a3fa9df62ac6d86dd4
indraindrajit71@gmail.com <indraindrajit71@gmail.com>
2024-05-22 23:11:08 +0530

    more-pynative-practice-python-day3

---
+++ Python-indra/input-output/inputfromuser.py
@@ -0,0 +1,12 @@
+def multi(n1,n2):
+    return(n1 * n2)
+
+def main():
+    number1=int(input("enter the number n1:"))
+    number2=int(input("enter the number n2:"))
+    result=multi(number1, number2)
+    print(result)
+if __name__== "__main__":
+    main()
+
+    
