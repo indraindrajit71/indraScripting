7c36856b51a45f8742903f6832b629a3d014a3c0
indraindrajit71@gmail.com <indraindrajit71@gmail.com>
2024-05-30 19:37:44 +0530

    more-pynative-practice-python-300524

---
+++ Python-indra/input-output/decimal_to_octal.py
@@ -0,0 +1,3 @@
+decimal_number=8
+oct_number=oct(decimal_number)
+print(oct_number)
---
+++ Python-indra/input-output/printfloating.py
@@ -0,0 +1,6 @@
+numbers = []
+
+for i in range(0,5):
+  int=float(input("enter the digits:"))
+  numbers.append(int)
+print("the floating are:", numbers )  
