eea0750ebef46856d10f564e550c81064b98a368
indraindrajit71@gmail.com <indraindrajit71@gmail.com>
2024-05-30 20:58:33 +0530

    more-pynative-practice-python-300524

---
+++ Python-indra/input-output/loops/print1-10.py
@@ -0,0 +1,10 @@
+for i in range(0,11):    ##using for loop
+    print(i,end=' ')
+
+
+print(list(range(0,11)))   ##using command line util
+
+i=0                        ##using whileloop
+while i<=10:
+    print(i)
+    i=i+1
